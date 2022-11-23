9cdc7b87d6618049be0022e3a03e238b90f63649
Aleksandr Litvinov <ssssank@gmail.com>
2022-11-24 00:09:25 +0500

    fix device

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
+  root to: 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts do
     scope module: :posts do
